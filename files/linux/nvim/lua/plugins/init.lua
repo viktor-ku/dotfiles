d62526493862edfdc19f456e9ea0064ab6aef0e0
Viktor Kuroljov <viktor@ku.family>
2022-12-20 08:54:08 +0200

    add creates to neovim

--- files/linux/nvim/lua/plugins/init.lua
+++ files/linux/nvim/lua/plugins/init.lua
@@ -110,6 +110,15 @@ return require('packer').startup({function(use)
     branch = 'v2', -- optional but strongly recommended
   }
 
+  use {
+    'saecki/crates.nvim',
+    tag = 'v0.3.0',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        require('crates').setup()
+    end,
+  }
+
 end, config = {
   package_root = vim.fn.stdpath('config') .. '/site/pack'
 }})
