37f57ef4a07e0c43a7e1e4f5c8575ce2843c93b7
Viktor Kuroljov <viktor@ku.family>
2025-09-23 01:00:17 +0300

    add xdg

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,5 +1,5 @@
-fish_add_path --append --path $HOME/.cargo/bin
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_CACHE_HOME $HOME/.cache
 
-set -Ux XDG_CONFIG_HOME $HOME/.config
-set -Ux XDG_DATA_HOME $HOME/.local/share
-set -Ux XDG_CACHE_HOME $HOME/.cache
+fish_add_path /opt/homebrew/opt/rustup/bin
