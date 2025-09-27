fad71d0adbda9678ce9be5fc651fb6951ab58c32
Viktor Kuroljov <viktor@ku.family>
2025-09-27 22:00:21 +0300

    nvim: add auto attach via nvim cmd

--- home/.config/nvim/lua/lsp_default.lua
+++ home/.config/nvim/lua/lsp_default.lua
@@ -1,37 +1,3 @@
-local function default_on_attach(client, bufnr)
-  -- Keymaps
-  local map = function(mode, lhs, rhs)
-    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true })
-  end
-
-  map("n", "gd", vim.lsp.buf.definition)
-
-  map("n", "K", vim.lsp.buf.hover)
-  map("n", "gd", vim.lsp.buf.definition)
-  map("n", "gD", vim.lsp.buf.declaration)
-  map("n", "gi", vim.lsp.buf.implementation)
-  map("n", "gt", vim.lsp.buf.type_definition)
-  map("n", "gr", vim.lsp.buf.references)
-
-  map("n", "<leader>rn", vim.lsp.buf.rename)
-  map("n", "<leader>ca", vim.lsp.buf.code_action)
-
-  map("n", "<C-k>", vim.lsp.buf.signature_help)
-
-  -- Inlay Hints
-  vim.lsp.inlay_hint.enable(true)
-
-  -- Show code lens if supported
-  if client.server_capabilities.codeLensProvider then
-    vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
-      buffer = bufnr,
-      callback = vim.lsp.codelens.refresh,
-    })
-
-    vim.lsp.codelens.refresh({ bufnr = 0 })
-  end
-end
-
 local function default_capabilities()
   --- @type lsp.ClientCapabilities
   local caps = vim.lsp.protocol.make_client_capabilities()
@@ -84,5 +50,22 @@ end
 vim.lsp.config("*", {
   root_markers = { ".git" },
   capabilities = default_capabilities(),
-  on_attach = default_on_attach,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local opts = {
+      buffer = args.buf,
+      silent = true,
+    }
+
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+  end,
 })
