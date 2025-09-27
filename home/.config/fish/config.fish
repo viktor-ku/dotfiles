9a4de9f6cbd8ce670f65870b3ec8c8223afb825b
Viktor Kuroljov <viktor@ku.family>
2025-09-27 22:00:06 +0300

    fish: replace autojump with zoxide

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,4 +2,13 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CACHE_HOME $HOME/.cache
 
-fish_add_path /opt/homebrew/opt/rustup/bin
+fish_add_path --append --path $HOME/.cargo/bin
+
+set -gx ANDROID_HOME $HOME/Android/Sdk
+fish_add_path --append --path $ANDROID_HOME/emulator
+fish_add_path --append --path $ANDROID_HOME/platform-tools
+fish_add_path --append --path $ANDROID_HOME/tools
+
+if type -q zoxide
+  zoxide init fish --cmd j | source
+end
