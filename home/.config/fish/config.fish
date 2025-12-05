9420f480ea059cf340b131eae592ba6e7d3ea036
Viktor Kuroljov <viktor@ku.family>
2025-12-06 00:51:20 +0200

    add screenshotting ability to hypr

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,6 +3,7 @@ set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CACHE_HOME $HOME/.cache
 
 fish_add_path --append --path $HOME/.cargo/bin
+fish_add_path --append --path $HOME/.local/bin
 
 set -gx ANDROID_HOME $HOME/Android/Sdk
 fish_add_path --append --path $ANDROID_HOME/emulator
