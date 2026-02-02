7e036ec566e78b4cdbee5495a1a670dc1c322739
Viktor Kuroljov <viktor.konsta@gmail.com>
2026-02-02 20:29:25 +0200

    a bunch of changes

--- home/.config/nvim/lua/shepack/tinted-nvim.lua
+++ home/.config/nvim/lua/shepack/tinted-nvim.lua
@@ -2,7 +2,7 @@ return {
   "tinted-theming/tinted-nvim",
   commit = "0347c54827694e85f4880233eefd71be3978e439",
   config = function()
-    local theme = "base24-0x96f"
+    local theme = "base24-ayu-light"
     require("tinted-colorscheme").setup(theme)
     pcall(vim.cmd.colorscheme, theme)
   end,
